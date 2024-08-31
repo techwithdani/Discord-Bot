6bdc5177fcf317da72c41baa183e1ea9b1c4bbeb
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-08-31 21:26:08 +0500

    Creating command handler

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
